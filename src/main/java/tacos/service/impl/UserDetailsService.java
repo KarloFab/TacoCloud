691152b897e51c8bd2b81c2345f061878577e850
KarloFab <fabijanickarlo1@gmail.com>
2019-10-14 19:10:49 +0200

    Added UserDetailsService

---
+++ src/main/java/tacos/service/impl/UserDetailsService.java
@@ -0,0 +1,28 @@
+package tacos.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tacos.domain.User;
+import tacos.repository.UserRepository;
+
+@Service
+public class UserDetailsService implements tacos.service.UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsService(UserRepository userRepo) {
+        this.userRepository = userRepo;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return user;
+        }
+        throw new UsernameNotFoundException(
+                "User '" + username + "' not found");
+    }
+}
