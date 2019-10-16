dfe9d193cd294dbfed9dccd7010e2053bf91c0d1
KarloFab <fabijanickarlo1@gmail.com>
2019-10-16 20:09:17 +0200

    Added WebConfig for Controllers that has only one aciton

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .hasRole("ROLE_USER")
+                .antMatchers("/", "/**").permitAll()
+
+        .and()
+                .formLogin()
+                .loginPage("/login");
+    }
 
     @Bean
     public PasswordEncoder encoder() {
