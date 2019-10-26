ab50d818eb5ff55c7629e376ea55f6ba3214fec1
KarloFab <fabijanickarlo1@gmail.com>
2019-10-26 15:09:28 +0200

    Added message converter

---
+++ src/main/java/tacos/config/messages/MessageConverter.java
@@ -0,0 +1,21 @@
+package tacos.config.messages;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import tacos.domain.Order;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessageConverter {
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter =
+                new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
+        typeIdMappings.put("order", Order.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+        return messageConverter;
+    }
+}
